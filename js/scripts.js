03a9d26e61b45f088a5b152d9c5b468c7e44032c
gabrielebottari <gabriele.bottari1@gmail.com>
2023-12-07 17:41:31 +0100

    ADD 1 bonus

--- js/scripts.js
+++ js/scripts.js
@@ -43,30 +43,70 @@ item[itemActive].classList.add('active');
 let previous = document.querySelector('.previous');
 let next = document.querySelector('.next');
 
+/*
 //prima funzione per il tasto next se itemactive non corrisponde all'ultimo elemento dell'array aumenta di 1
 next.addEventListener ('click', function() {
 
+    //cliccando tolgo il display block
     item[itemActive].classList.remove('active');
 
     if ( itemActive != imgArray.length-1 ) {
         itemActive++;
     }
     
+    //a prescidere se il ciclo si sposta o meno alla immagine successiva metterà nuovamente il display-block
     item[itemActive].classList.add('active');
 
 });
+*/
 
+/*
 //seconda funzione per il tasto previous se itemactive non corrisponde al primo elemento dell'array diminuirà di 1
 previous.addEventListener ('click', function() {
-
+    
+    //cliccando tolgo il display block
     item[itemActive].classList.remove('active');
 
     if (itemActive != 0) {
         itemActive--;
     }
     
+    //a prescidere se il ciclo si sposta o meno alla immagine successiva metterà nuovamente il display-block
+    item[itemActive].classList.add('active');
+
+});
+*/
+
+// prima funzione per il tasto next al click rimuove active all'item corrispondente, se l'item è l'ultimo lo riporta al primo
+// sennò avanzerà di uno, alla fine della funzione rimetterà active all'item corrispondente.
+next.addEventListener ('click', function() {
+
+    item[itemActive].classList.remove('active');
+
+    if ( itemActive == imgArray.length-1 ) {
+        itemActive = 0;
+    }
+    else {
+        itemActive++;
+    };
+
     item[itemActive].classList.add('active');
 
 });
 
+// seconda funzione per il tasto previous al click rimuove active all'item corrispondente, se l'item è il primo lo riporta all'ultimo
+// sennò diminuirà di uno, alla fine della funzione rimetterà active all'item corrispondente.
+previous.addEventListener ('click', function() {
 
+    item[itemActive].classList.remove('active');
+
+    if ( itemActive == 0 ) {
+        itemActive = imgArray.length - 1;
+    }
+    else {
+        itemActive--;
+    };
+
+    item[itemActive].classList.add('active');
+
+});
