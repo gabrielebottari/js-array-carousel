e448dd54d4476aa81347e6ba9b05ae98f7a696bc
gabrielebottari <gabriele.bottari1@gmail.com>
2023-12-07 15:52:37 +0100

    ADD 2 milenstone

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,33 @@
-// Test funzionamento
-console.log('Template HTML CSS JS');
+/*
+MILESTONE 2
+Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal.
+Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
+Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
+*/
+
+const imgArray = [
+    '01.webp',
+    '02.webp',
+    '03.webp',
+    '04.webp',
+    '05.webp',
+];
+
+let itemContent = '';
+
+for (let i = 0; i < imgArray.length; i++) {
+    itemContent += `<div class="item"><img src="./img/${ imgArray[i] }"></div>`; 
+};
+
+const itemsSlider = document.querySelector('.item-slider').innerHTML = itemContent;
+
+const item = document.getElementsByClassName('item');
+
+let itemActive = 0;
+
+item[itemActive].classList.add('active');
+
+let previous = document.querySelector('.previous');
+let next = document.querySelector('.next');
+
+
