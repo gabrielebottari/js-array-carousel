e29eccd438fdfc96a2278cd964d54d5d4a0cbf45
gabrielebottari <gabriele.bottari1@gmail.com>
2023-12-08 14:21:30 +0100

    ADD bonus 2

--- js/scripts.js
+++ js/scripts.js
@@ -19,19 +19,37 @@ const imgArray = [
     '05.webp',
 ];
 
+
+// Creazione array bottoni
+const btnArray = [
+    '01.webp',
+    '02.webp',
+    '03.webp',
+    '04.webp',
+    '05.webp',
+];
+
 // inizializzo variabile per rappresentare il contenuto del carosello
 let itemContent = '';
+let itemThumbnail = '';
 
 // ciclo for con array e per ogni i dell'array creo un div con un img nell'ordine corrispondente
+// creo anche un button con una immagine dentro
 for (let i = 0; i < imgArray.length; i++) {
-    itemContent += `<div class="item"><img src="./img/${imgArray[i]}"></div>`; 
+    itemContent += `<div class="item"><img src="./img/${imgArray[i]}" alt="Image ${i+1}"></div>`; 
+    itemThumbnail += `<button type="button" onclick="showImage(${i})"><img src="./img/${imgArray[i]}" alt="Image ${i+1}"></button>`; 
 };
 
-// scrivo inserisco nell'html il codice js creato con il ciclo for
-const itemsSlider = document.querySelector('.item-slider').innerHTML = itemContent;
 
-// inizializzo una costante e le do la classe
+// inserisco nell'html il codice js creato con il ciclo for
+const itemsSlider = document.querySelector('.item-slider').innerHTML = itemContent;
+const itemsThumbnail = document.querySelector('.thumbnail-container').innerHTML = itemThumbnail;
+
+// inizializzo una costante e l'associo a chia ha la classe selezionata
 const item = document.getElementsByClassName('item');
+
+// inizializzo una costante e l'asscocio a tutte le query con questo selettore avanzato
+const thumbnails = document.querySelectorAll('.thumbnail-container button');
 
 // inizializzo una variabile per rappresentare l'immagine che dovrà essere visibile in questo caso la prima dell'array
 let itemActive = 0;
@@ -110,3 +128,13 @@ previous.addEventListener ('click', function() {
     item[itemActive].classList.add('active');
 
 });
+
+// terza funzione per i bottoni rimuovo la classe active e lo assegno alla stessa immagine che corrisponde
+//  all'indice del bottone corrispondente in seguito darò a quell'imagine classe active
+function showImage(index) {
+
+    item[itemActive].classList.remove('active');
+    itemActive = index;
+    item[itemActive].classList.add('active');
+
+}
